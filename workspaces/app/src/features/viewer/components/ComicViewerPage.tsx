452c0f306b7125f392182cfaccc953f17fb8301a
yu-3in <misharv.y@gmail.com>
2024-03-24 03:36:19 +0900

    漫画の画像をwebpに変更

--- workspaces/app/src/features/viewer/components/ComicViewerPage.tsx
+++ workspaces/app/src/features/viewer/components/ComicViewerPage.tsx
@@ -23,7 +23,7 @@ export const ComicViewerPage = ({ pageImageId }: Props) => {
   useAsync(async () => {
     const image = new Image();
     image.src = getImageUrl({
-      format: 'jpg',
+      format: 'webp',
       imageId: pageImageId,
     });
     await image.decode();
