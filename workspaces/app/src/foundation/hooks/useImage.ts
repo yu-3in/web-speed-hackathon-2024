69f8c5978dfa40d4db5acb4698dec4db4887b8de
yu-3in <misharv.y@gmail.com>
2024-03-23 18:39:46 +0900

    画像URLの不要なStateを削除

--- workspaces/app/src/foundation/hooks/useImage.ts
+++ workspaces/app/src/foundation/hooks/useImage.ts
@@ -1,24 +1,11 @@
-import { useEffect, useState } from 'react';
-
 import { getImageUrl } from '../../lib/image/getImageUrl';
 
 export const useImage = ({ height, imageId, width }: { height: number; imageId: string; width: number }) => {
-  const [imageUrl, setImageUrl] = useState<string>();
-
-  useEffect(() => {
-    const fetchImageUrl = async () => {
-      const dpr = window.devicePixelRatio;
-      const url = getImageUrl({
-        format: 'jpg',
-        height: height * dpr,
-        imageId,
-        width: width * dpr,
-      });
-      setImageUrl(url);
-    };
-
-    fetchImageUrl();
-  }, [height, imageId, width]);
-
-  return imageUrl;
+  const dpr = window.devicePixelRatio;
+  return getImageUrl({
+    format: 'jpg',
+    height: height * dpr,
+    imageId,
+    width: width * dpr,
+  });
 };
