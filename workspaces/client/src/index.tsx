d5f0e3c8a1110d3c095d610e65802d34df1bf8a5
yu-3in <misharv.y@gmail.com>
2024-03-24 05:58:32 +0900

    ClientとAdminをsplit

--- workspaces/client/src/index.tsx
+++ workspaces/client/src/index.tsx
@@ -2,9 +2,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { SWRConfig } from 'swr';
 
-import { AdminApp } from '@wsh-2024/admin/src/index';
-import { ClientApp } from '@wsh-2024/app/src/index';
-
 // import { preloadImages } from './utils/preloadImages';
 import { registerServiceWorker } from './utils/registerServiceWorker';
 
@@ -16,8 +13,10 @@ const main = async () => {
   if (!rootElement) return;
 
   if (window.location.pathname.startsWith('/admin')) {
+    const { AdminApp } = await import('@wsh-2024/admin/src/index');
     ReactDOM.createRoot(rootElement).render(<AdminApp />);
   } else {
+    const { ClientApp } = await import('@wsh-2024/app/src/index');
     ReactDOM.hydrateRoot(
       rootElement,
       <SWRConfig value={{ revalidateIfStale: true, revalidateOnFocus: false, revalidateOnReconnect: false }}>
