05eb673574c5fb2d2f2f5603cf1c752514773ab4
yu-3in <misharv.y@gmail.com>
2024-03-24 06:00:31 +0900

    Revert "Revert "キャッシュ復活""
    This reverts commit a7f1fad5ac7abb90c7a6d4e7813c873ea2473eac.

--- workspaces/server/src/middlewares/cacheControlMiddleware.ts
+++ workspaces/server/src/middlewares/cacheControlMiddleware.ts
@@ -2,6 +2,13 @@ import { createMiddleware } from 'hono/factory';
 
 export const cacheControlMiddleware = createMiddleware(async (c, next) => {
   await next();
-  c.res.headers.append('Cache-Control', 'private');
-  c.res.headers.append('Cache-Control', 'no-store');
+
+  if (new URL(c.req.url).pathname.startsWith('/assets/')) {
+    c.res.headers.append('Cache-Control', 'public');
+    c.res.headers.append('Cache-Control', 'max-age=31536000');
+    return;
+  } else {
+    c.res.headers.append('Cache-Control', 'private');
+    c.res.headers.append('Cache-Control', 'no-store');
+  }
 });
--- workspaces/server/src/routes/index.ts
+++ workspaces/server/src/routes/index.ts
@@ -4,6 +4,8 @@ import { cors } from 'hono/cors';
 import { HTTPException } from 'hono/http-exception';
 import { secureHeaders } from 'hono/secure-headers';
 
+import { cacheControlMiddleware } from '../middlewares/cacheControlMiddleware';
+
 import { adminApp } from './admin';
 import { apiApp } from './api';
 import { imageApp } from './image';
@@ -24,6 +26,7 @@ app.use(
 );
 
 app.use(compress());
+app.use(cacheControlMiddleware);
 
 app.get('/healthz', (c) => {
   return c.body('live', 200);
