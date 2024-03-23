da8c6ec79fbb6d0067d82061938bc180032023d9
yu-3in <misharv.y@gmail.com>
2024-03-23 17:47:03 +0900

    サービスワーカーでjxlを変換しなくなったので、jpgで取得するように変更

--- workspaces/app/src/features/viewer/components/ComicViewerPage.tsx
+++ workspaces/app/src/features/viewer/components/ComicViewerPage.tsx
@@ -23,7 +23,7 @@ export const ComicViewerPage = ({ pageImageId }: Props) => {
   useAsync(async () => {
     const image = new Image();
     image.src = getImageUrl({
-      format: 'jxl',
+      format: 'jpg',
       imageId: pageImageId,
     });
     await image.decode();
